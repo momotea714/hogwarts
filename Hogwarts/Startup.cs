2fe4baf6d05f07041dd16c588c667db9ae14e4e8
momo <momotea714@gmail.com>
2018-01-20 16:48:05 +0900

    プロジェクト ファイルを追加します。

---
+++ Hogwarts/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Hogwarts.Startup))]
+namespace Hogwarts
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
